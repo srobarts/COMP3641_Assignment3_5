827e833d168e1ea82eb31642a7ef438a8d71b703
Scott Robarts <scott.robarts@blinkdesigns.ca>
2011-07-01 19:47:00 -0700

    working code using only an arraylist for data

--- src/com/assmt3/core/TableData.java
+++ src/com/assmt3/core/TableData.java
@@ -1,6 +1,9 @@
 package com.assmt3.core;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.faces.bean.ManagedBean; 
    // or import javax.inject.Named;
 import javax.faces.bean.SessionScoped; 
@@ -11,18 +14,23 @@ import javax.faces.bean.SessionScoped;
 public class TableData implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private static final Customer[] customers = new Customer[] {
+	private ArrayList<Customer> customers = new ArrayList<Customer>(Arrays.asList(
 		new Customer("Anna", "Keeney", "100 Lonsdale", "654-333-1212"),
 	    new Customer("John", "Wilson", "123 Marine Drive", "604-555-1234"),
 	    new Customer("Mariko", "Randor", "555 Howe Street", "555-232-9045"),
 	    new Customer("William", "Dupont", "345 Granville Street", "345-453-1234")
-	};
+	));
    
-   public Customer[] getCustomers() { return customers;}
+   public ArrayList<Customer> getCustomers() { return customers;}
 
    public String save() {
       for (Customer customer : customers) customer.setEditable(false);
       return null;
    }
+   
+   public String deleteRow(Customer nameToDelete) {
+	      customers.remove(nameToDelete);
+	      return null;
+	   }
    
 }
