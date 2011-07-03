66699664f80b56f87889d797c2f6533f9d400376
Scott Robarts <scott.robarts@blinkdesigns.ca>
2011-07-02 23:51:56 -0700

    added validation, styling, rowStyles, jarred up file

--- src/com/assmt3/core/TableData.java
+++ src/com/assmt3/core/TableData.java
@@ -24,7 +24,12 @@ public class TableData implements Serializable {
 		new Customer(1, "Anna", "Keeney", "100 Lonsdale", "North Vancouver", "BC", "654-333-1212"),
 	    new Customer(2, "John", "Wilson", "123 Marine Drive", "North Vancouver", "BC", "604-555-1234"),
 	    new Customer(3, "Mariko", "Randor", "555 Howe Street", "Vancouver", "BC", "555-232-9045"),
-	    new Customer(4, "William", "Dupont", "345 Granville Street", "Vancouver", "BC", "345-453-1234")
+	    new Customer(5, "William", "Dupont", "345 Granville Street", "Vancouver", "BC", "345-453-1234"),
+		new Customer(6, "Bill", "Dupont", "500 Granville Street", "Vancouver", "BC", "345-453-1234"),
+		new Customer(7, "Sue", "Williams", "820 Hornby Street", "Vancouver", "BC", "345-453-1234"),
+		new Customer(8, "Joseph", "Johns", "120 Deercrest Ave", "North Vancouver", "BC", "345-453-1234"),
+		new Customer(9, "Phil", "Hartman", "45 Georgia Street", "Vancouver", "BC", "345-453-1234"),
+		new Customer(10, "Jane", "Smith", "90 Davie Street", "Vancouver", "BC", "345-453-1234")
 	));
    
    //public ArrayList<Customer> getCustomers() { return customers;}
@@ -57,6 +62,42 @@ public class TableData implements Serializable {
       });
       return null;
    }
+   
+   public String sortByAddress() {
+	      filterModel.sortBy(new Comparator<Customer>() {
+	         public int compare(Customer n1, Customer n2) {
+	            return n1.getAddress().compareTo(n2.getAddress());
+	         }  
+	      });
+	      return null;
+	   }
+   
+   public String sortByCity() {
+	      filterModel.sortBy(new Comparator<Customer>() {
+	         public int compare(Customer n1, Customer n2) {
+	            return n1.getCity().compareTo(n2.getCity());
+	         }  
+	      });
+	      return null;
+	   }
+   
+   public String sortByProv() {//
+	      filterModel.sortBy(new Comparator<Customer>() {
+	         public int compare(Customer n1, Customer n2) {
+	            return n1.getProv().compareTo(n2.getProv());
+	         }  
+	      });
+	      return null;
+	   }
+   
+   public String sortByPhone() {
+	      filterModel.sortBy(new Comparator<Customer>() {
+	         public int compare(Customer n1, Customer n2) {
+	            return n1.getPhone().compareTo(n2.getPhone());
+	         }  
+	      });
+	      return null;
+	   }
 
    public String save() {
       for (Customer customer : customers) customer.setEditable(false);
